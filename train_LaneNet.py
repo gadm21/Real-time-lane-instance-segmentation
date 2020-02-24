6b45e8d8a14a405bc057cd6c2d799db331919429
gad <g.gad@nu.edu.eg>
2020-02-24 03:56:22 +0200

    third commit

--- train_LaneNet.py
+++ train_LaneNet.py
@@ -4,10 +4,16 @@
 def train_LaneNet (dataset_dir, weights_path):
 
     ''' waiting
+    first guess:: this function takes the dataset directory and returns clean chunks, 
+    ready to be used in the network (for training or validation) depending on the 
+    batch size and epochs_num that is passed later to LaneNetDataFeeder.input
+
     train_dataset= LaneNet_data_feed_pipeline.LaneNetDataFeeder(dataset_dir, flags= 'train')
     val_dataset= LaneNet_data_feed_pipeline.LaneNetDataFeeder(dataset_dir, flags= 'val')
     '''
 
 
-    with tf.device('/gpu:1'):
-        
+    train_net= LaneNet.LaneNet(phase= 'train', reuse= False)
+    val_net= LaneNet.LaneNet(phase= 'val', reuse= True)
+
+

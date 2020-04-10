3d47fd8804c167da4a98626db23895c982032811
unknown <g.gad@nu.edu.eg>
2020-04-10 14:07:04 +0200

    clean files

---
+++ LaneNet/LaneNet_model/my_postprocessor.py
@@ -0,0 +1,290 @@
+
+
+import cv2 
+import numpy as np 
+from sklearn.cluster import DBSCAN
+import os 
+import sys 
+import random
+random.seed(4)
+sys.path.append(os.getcwd()) 
+
+def read_image(image_path):
+    return cv2.imread(image_path) 
+
+def save_image(save_dir, image_name, image):
+    os.makedirs(save_dir, exist_ok=True) 
+    cv2.imwrite(os.path.join(save_dir, image_name+".png"), image)
+
+def show_image(image, label= 'r' ):
+
+    cv2.imshow(label, image) 
+    cv2.waitKey(0)  
+    cv2.destroyWindow(label)
+
+def to_gray(image):
+
+    if len(image.shape) == 3:
+        gray_image= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else: gray_image= image 
+
+    return gray_image 
+
+def to_colored(image):
+    if len(image.shape) < 3:
+        colored_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    else: colored_image= image
+
+    return colored_image 
+
+def remove_noise(image, min_area_threshold= 500):
+    assert len(image.shape) != 3, "remove noise accepts gray images only"
+    _, labels, stats, _= cv2.connectedComponentsWithStats(image, connectivity= 8, ltype= cv2.CV_32S)
+
+    for index, stat in enumerate(stats):
+        if stat[4] < min_area_threshold:
+            noise_indecies= np.where(labels == index)
+            image[noise_indecies] = 0
+
+    return image 
+
+def morphological_process(image, kernel_size= 5):
+    assert len(image.shape) != 3, "morphological_process accepts gray images only"
+
+    kernel= cv2.getStructuringElement(shape= cv2.MORPH_ELLIPSE, ksize= (kernel_size, kernel_size)) 
+    filled_holes_image= cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations= 1)
+
+    return filled_holes_image 
+
+def resize_image(image, new_size):
+    return cv2.resize(image, new_size)
+
+
+
+
+class Lane(object):
+
+    def __init__(self, id=0):
+        self.clusters = []
+        self.means= [] 
+        self.window_h= 5 
+        self.id= id 
+        self.valid= True 
+        self.image_h= None 
+        self.image_w= None 
+        self.remap_to_x= None 
+        self.remap_to_y= None 
+    
+    def mean(self):
+        current_mean= (int(np.mean(self.clusters[-1][0])), int(np.mean(self.clusters[-1][1])))
+        prev_mean= self.means[-1]
+
+        mean_change= (current_mean[0] - prev_mean[0], current_mean[1] - prev_mean[1]) 
+        predicted_mean= (current_mean[0] + mean_change[0] , current_mean[1] + mean_change[1])
+        self.means.append(predicted_mean) 
+
+        return predicted_mean    
+
+    def print_info(self):
+        print("lane {:d} info:".format(self.id))
+        print("number of pixels on this lane == {:d}".format(self.num_points()))
+        print("average cluster width == {:f}".format(self.cluster_width()))
+        
+        print("mumber of clusters == {:d}".format(len(self.clusters)))
+        print("____________________________________________")
+        print() 
+
+    def num_points(self):
+        total= 0
+        for cluster in self.clusters:
+            total+= cluster[0].shape[0]
+        return int(total)  
+    
+    def draw_mask(self, shape= None, color_means= False):
+        if shape is None:
+            shape= ( self.image_w, self.image_h  ) 
+
+        mask= np.zeros(shape= (self.image_h ,self.image_w), dtype= np.uint8)
+        mask= self.colorize(mask, 255, color_means= color_means) 
+        mask= resize_image(mask, shape) 
+        return mask 
+
+    def colorize(self, image, color, color_means= True):
+        print ("lane {:d} is colorizing".format(self.id))
+
+        if color_means:
+            for mean in self.means:
+                cv2.circle(image, (mean[1], mean[0]), 1, color, 2)
+
+        else:
+            for cluster in self.clusters:
+                image[cluster]= color 
+            
+
+        return image 
+
+    def cluster_width(self):
+        total= 0
+        for cluster in self.clusters:
+            low_x = np.min(cluster[1])
+            high_x= np.max(cluster[1])       
+            diff= high_x - low_x
+            total+= diff 
+        
+        return int(total // len(self.clusters) )
+
+    def blacken(self, image) :
+        for cluster in self.clusters:
+            image[cluster] = 0 
+        
+        return image 
+
+    def complete(self, cluster_coords, image):
+
+        self.clusters.append(cluster_coords) 
+        self.means.append((int(np.mean(self.clusters[-1][0])), int(np.mean(self.clusters[-1][1]))))
+
+        self.image_h, self.image_w = image.shape[0], image.shape[1]
+        lanes_coords= np.where(image == 255) 
+        lowest_lane_coord= np.min(lanes_coords[0])
+        highest_lane_coord= np.max(cluster_coords[0]) 
+        window_center= self.means[-1][1]
+        
+        
+        for window in range(highest_lane_coord, lowest_lane_coord, - self.window_h):
+            margin= int(self.cluster_width())
+
+            window_pix= (lanes_coords[0] >= window - self.window_h) & \
+                        (lanes_coords[0] < window) & \
+                        (lanes_coords[1] > (window_center - margin)) & \
+                        (lanes_coords[1] < (window_center + margin))
+            lane_coords_within_window = (lanes_coords[0][window_pix], lanes_coords[1][window_pix])
+            if lane_coords_within_window[0].shape[0] == 0 : continue  
+
+            self.clusters.append(lane_coords_within_window) 
+            window_center= self.mean()[1]
+
+        image= self.blacken(image) 
+        image= remove_noise(image) 
+        return image 
+             
+    def load_remap_matrix(self, remap_file_path):
+        if not os.path.exists(remap_file_path):
+            return False
+
+        fs= cv2.FileStorage(remap_file_path, cv2.FILE_STORAGE_READ)
+
+        self.remap_to_x= fs.getNode('remap_ipm_x').mat() 
+        self.remap_to_y= fs.getNode('remap_ipm_y').mat()
+
+        fs.release() 
+
+        return True 
+        
+
+    def fit(self, remap_file_path):
+        mask= self.draw_mask(color_means= True) 
+
+        if self.load_remap_matrix(remap_file_path):
+            ipm_mask= cv2.remap(mask, self.remap_to_x, \
+                self.remap_to_y, interpolation= cv2.INTER_NEAREST)
+            
+            return mask, ipm_mask 
+
+
+
+class PostProcessor(object):
+
+    def __init__(self):
+        
+        self.stride_h= -5
+        self.lane_id= 0
+        
+        self.color_map= [[0, 0, 255],
+                        [0, 255, 0],
+                        [255, 0, 0],
+                        [255, 255, 0]]
+
+
+        self.dbscan_eps= 8
+        self.dbscan_min_samples= 30
+        self.db= DBSCAN(self.dbscan_eps, self.dbscan_min_samples) 
+        self.lane_acceptance_factor= 0.4
+    
+    def give_id(self):
+        self.lane_id+= 1
+        return self.lane_id 
+
+    def pre_processing(self, image):
+        image= to_gray(image) 
+        image= remove_noise(image) 
+        image= morphological_process(image) 
+        return image 
+
+    def inspect_lanes(self, lanes):
+        total_points= 0
+        for lane in lanes:
+            total_points+= lane.num_points()
+        average_lane_points= total_points / len(lanes)
+        min_lane_points= average_lane_points * self.lane_acceptance_factor
+
+        for lane in lanes:
+            if lane.num_points() < min_lane_points:
+                lane.valid= False 
+        
+    def apply_clustering_on_stride(self, coords):
+
+        ret= self.db.fit(np.array(coords).transpose())
+        labels= ret.labels_
+        unique_labels= np.unique(labels) 
+        return labels, unique_labels 
+
+    def post_process(self, binary, source):
+        
+        source= resize_image(source, (binary.shape[1], binary.shape[0]))
+        image= self.pre_processing(binary) 
+
+        image_h, image_w = image.shape
+        lanes_coords= np.where(image == 255) 
+        lowest_lane_coord= np.min(lanes_coords[0])
+        highest_lane_coord= np.max(lanes_coords[0]) 
+        
+        
+        lanes= []
+        for stride in range(highest_lane_coord, lowest_lane_coord, self.stride_h):
+            lanes_coords= np.where(image == 255)
+            target_within_stride= (lanes_coords[0] < stride) & (lanes_coords[0] >= (stride + self.stride_h))
+            stride_lanes_coords= (lanes_coords[0][target_within_stride], lanes_coords[1][target_within_stride])
+            
+            if stride_lanes_coords[0].shape[0] == 0 : continue 
+
+            labels, unique_labels= self.apply_clustering_on_stride(stride_lanes_coords) 
+            for label in unique_labels:
+                if label==-1:  continue             
+                cluster= (labels == label)
+                cluster_coords= (stride_lanes_coords[0][cluster], stride_lanes_coords[1][cluster])
+                
+                lane= Lane(self.give_id()) 
+                image= lane.complete(cluster_coords, image) 
+                lanes.append(lane) 
+                
+        self.inspect_lanes(lanes) 
+        
+        for lane in lanes:
+            if not lane.valid: 
+                continue  
+            m, i= lane.fit('tusimple_ipm_remap.yml')
+            show_image(m, label= str(lane.id))
+            show_image(i, label= str(lane.id))
+            mask |= m
+            ipm_mask |= i
+            
+            
+        return mask, ipm_mask 
+        
+        
+
+
+
+
+
---
+++ train_eval_test/train_LaneNet.py
@@ -0,0 +1,181 @@
+
+import argparse
+import math
+import time
+import cv2
+import glog as log
+import numpy as np
+import tensorflow as tf
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+from LaneNet.data_provider import LaneNet_data_feed_pipeline
+from LaneNet.LaneNet_model import LaneNet
+from tools.evaluate_model import calculate_model_precision, calculate_model_fp, calculate_model_fn, get_image_summary
+import global_config
+cfg= global_config.cfg 
+
+
+
+def init_args():
+    """
+
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--dataset_dir', type=str, help='Lanenet Dataset dir')
+    parser.add_argument('-w', '--weights_path', type=str, default= None, help='Path to pre-trained weights to continue training')
+
+    #parser.add_argument('--net_flag', type=str, default='vgg', help='The net flag which determins the net\'s architecture')
+
+    return parser.parse_args()
+
+
+
+
+
+
+def train_LaneNet (dataset_dir, weights_path= None):
+
+ 
+    train_dataset= LaneNet_data_feed_pipeline.LaneNetDataFeeder(dataset_dir, flags= 'train')
+    val_dataset= LaneNet_data_feed_pipeline.LaneNetDataFeeder(dataset_dir, flags= 'val')
+    
+
+
+    train_net= LaneNet.LaneNet(phase= 'train', reuse= False)
+    val_net= LaneNet.LaneNet(phase= 'val', reuse= True)
+
+
+    train_images, train_binary_labels, train_instance_labels= train_dataset.inputs(cfg.TRAIN.BATCH_SIZE, 1)
+    
+    print("train_images:", train_images.shape, "...................................")
+    print("binary_labels:", train_binary_labels.shape, "...........................")
+    print("instance_labels:", train_instance_labels.shape, ".......................")
+    
+    #compute loss 
+    train_compute_ret= train_net.compute_loss(input_tensor= train_images, 
+                                            binary_label= train_binary_labels,
+                                            instance_label= train_instance_labels,
+                                            name= 'lanenet_model')
+
+    train_total_loss= train_compute_ret['total_loss']
+    train_binary_seg_loss= train_compute_ret['binary_seg_loss']
+    train_discriminative_loss= train_compute_ret['discriminative_loss']
+
+
+    #compute accuracy
+    train_pix_embedding= train_compute_ret['instance_seg_logits']    
+    train_prediction_logits= train_compute_ret['binary_seg_logits']
+    train_prediction_score= tf.nn.softmax(logits= train_prediction_logits)
+    train_prediction= tf.argmax(train_prediction_score, axis= -1)
+    train_accuracy= calculate_model_precision(train_prediction_logits, train_binary_labels)
+    train_fp= calculate_model_fp(train_prediction_logits, train_binary_labels)
+    train_fn= calculate_model_fn(train_prediction_logits, train_binary_labels)
+
+
+    #summary
+    train_binary_seg_ret_for_summary = get_image_summary( img=train_prediction )
+    train_embedding_ret_for_summary = get_image_summary( img=train_pix_embedding )
+
+    train_cost_scalar = tf.summary.scalar(
+        name='train_cost', tensor=train_total_loss
+    )
+    train_accuracy_scalar = tf.summary.scalar(
+        name='train_accuracy', tensor=train_accuracy
+    )
+    train_binary_seg_loss_scalar = tf.summary.scalar(
+        name='train_binary_seg_loss', tensor=train_binary_seg_loss
+    )
+    train_instance_seg_loss_scalar = tf.summary.scalar(
+        name='train_instance_seg_loss', tensor=train_discriminative_loss
+    )
+    train_fn_scalar = tf.summary.scalar(
+        name='train_fn', tensor=train_fn
+    )
+    train_fp_scalar = tf.summary.scalar(
+        name='train_fp', tensor=train_fp
+    )
+    train_binary_seg_ret_img = tf.summary.image(
+        name='train_binary_seg_ret', tensor=train_binary_seg_ret_for_summary
+    )
+    train_embedding_feats_ret_img = tf.summary.image(
+        name='train_embedding_feats_ret', tensor=train_embedding_ret_for_summary
+    )
+    train_merge_summary_op = tf.summary.merge(
+        [train_accuracy_scalar, train_cost_scalar, train_binary_seg_loss_scalar,
+            train_instance_seg_loss_scalar, train_fn_scalar, train_fp_scalar,
+            train_binary_seg_ret_img, train_embedding_feats_ret_img]
+    )
+
+
+    # set tensorflow saver
+    saver = tf.train.Saver()
+    model_save_dir = 'model'
+    os.makedirs(model_save_dir, exist_ok=True)
+    train_start_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+    model_name = 'tusimple_lanenet_{:s}.ckpt'.format( str(train_start_time))
+    model_save_path = os.path.join(model_save_dir, model_name)
+
+
+    #set tensorboard
+    tboard_save_path = 'tboard/logs'
+    os.makedirs(tboard_save_path, exist_ok=True)
+
+
+
+    #set optimizer
+    global_step= tf.Variable(0, trainable= False)
+    learning_rate= tf.train.polynomial_decay(learning_rate= cfg.TRAIN.LEARNING_RATE,global_step=global_step,decay_steps=cfg.TRAIN.EPOCHS, power= 0.9)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):    
+        optimizer= tf.train.MomentumOptimizer(learning_rate= learning_rate, momentum= cfg.TRAIN.MOMENTUM).minimize(loss= train_total_loss, var_list= tf.trainable_variables(), global_step= global_step)
+
+    saver= tf.train.Saver()
+    train_epochs= cfg.TRAIN.EPOCHS
+    sess= tf.Session()
+    summary_writer = tf.summary.FileWriter(tboard_save_path)
+    summary_writer.add_graph(sess.graph)
+
+    with sess.as_default():
+        
+        if weights_path is not None:
+            saver.restore(sess= sess, save_path= weights_path)
+        else: sess.run(tf.global_variables_initializer())
+
+        train_cost_time_mean= []
+        for epoch in range(train_epochs):
+            train_start_time= time.time()
+            
+            try:    
+                _, train_c, train_accuracy_figure, lr, train_binary_loss, train_instance_loss,train_embeddings, train_binary_seg_imgs, train_gt_imgs, train_binary_gt_labels, train_instance_gt_labels, train_summary = \
+                    sess.run([optimizer, train_total_loss, train_accuracy, learning_rate, train_binary_seg_loss, train_discriminative_loss, train_pix_embedding, train_prediction, train_images, train_binary_labels, train_instance_labels, train_merge_summary_op])            
+            except tf.errors.InvalidArgumentError: break
+
+            summary_writer.add_summary(summary=train_summary, global_step=epoch)
+
+            '''
+            if epoch % 10 == 0:
+                record_training_intermediate_result(
+                    gt_images=train_gt_imgs, gt_binary_labels=train_binary_gt_labels,
+                    gt_instance_labels=train_instance_gt_labels, binary_seg_images=train_binary_seg_imgs,
+                    pix_embeddings=train_embeddings
+                )
+            '''
+            print("accuracy:", train_c)
+            print("loss:", train_binary_loss)
+
+        print("saving model")
+        saver.save(sess=sess, save_path=model_save_path, global_step=global_step)
+
+    sess.close()
+
+
+if __name__ == '__main__':
+    # init args
+    args = init_args()
+    dataset_dir= r"C:\Users\gad\Downloads\Compressed\lanenet-lane-detection\data\data_records"
+    train_LaneNet(dataset_dir, args.weights_path) 
